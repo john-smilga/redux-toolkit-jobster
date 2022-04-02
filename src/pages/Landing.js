75213cc2ea8c641449a156d41462872d54dcb6de
john smilga <johnsmilga@gmail.com>
2022-04-02 12:01:18 -0700

    5-landing-structure-complete

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,6 +2,27 @@ import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
 
 const Landing = () => {
-  return <h1>Landing page</h1>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt='jobster logo' className='logo' />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Crucifix narwhal street art asymmetrical, humblebrag tote bag pop-up
+            fixie raclette taxidermy craft beer. Brunch bitters synth, VHS
+            crucifix heirloom meggings bicycle rights.
+          </p>
+          <button className='btn btn-hero'>Login/Register</button>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </main>
+  );
 };
 export default Landing;
