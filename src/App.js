cf59e887e2e7905e7b0122bb17f317cc0c26d7a3
john smilga <johnsmilga@gmail.com>
2022-04-03 11:39:49 -0700

    21-react-toastify-complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Landing, Error, Dashboard, Register } from './pages';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Route path='register' element={<Register />} />
         <Route path='*' element={<Error />} />
       </Routes>
+      <ToastContainer position='top-center' />
     </BrowserRouter>
   );
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Logo, FormRow } from '../components';
 import Wrapper from '../assets/wrappers/RegisterPage';
-
+import { toast } from 'react-toastify';
 const initialState = {
   name: '',
   email: '',
@@ -22,7 +22,7 @@ function Register() {
     e.preventDefault();
     const { name, email, password, isMember } = values;
     if (!email || !password || (!isMember && !name)) {
-      console.log('Please fill out all fields');
+      toast.error('Please fill out all fields');
     }
   };
 
