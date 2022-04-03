df6a57ac23aead67377836f756ab6675badf3303
john smilga <johnsmilga@gmail.com>
2022-04-03 09:51:42 -0700

    17-switch-to-react-18-complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App tab='home' />);
