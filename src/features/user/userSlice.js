fbc13ead2dc067ff2cb4a9b341580feba38b5c5c
john smilga <johnsmilga@gmail.com>
2022-04-04 14:07:33 -0700

    38-logout-user

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -44,6 +44,11 @@ const userSlice = createSlice({
     toggleSidebar: (state) => {
       state.isSidebarOpen = !state.isSidebarOpen;
     },
+    logoutUser: (state) => {
+      state.user = null;
+      state.isSidebarOpen = false;
+      removeUserFromLocalStorage();
+    },
   },
   extraReducers: {
     [registerUser.pending]: (state) => {
@@ -78,5 +83,5 @@ const userSlice = createSlice({
   },
 });
 
-export const { toggleSidebar } = userSlice.actions;
+export const { toggleSidebar, logoutUser } = userSlice.actions;
 export default userSlice.reducer;
