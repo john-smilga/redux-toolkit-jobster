657cf92c3187bfe04a6d9bb686a7eb07cc60f89e
john smilga <johnsmilga@gmail.com>
2022-04-04 15:09:45 -0700

    45-big-sidebar

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,9 +1,27 @@
+import NavLinks from './NavLinks';
+import Logo from '../components/Logo';
 import Wrapper from '../assets/wrappers/BigSidebar';
+import { useSelector } from 'react-redux';
 
 const BigSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
+
   return (
     <Wrapper>
-      <h2>BigSidebar</h2>
+      <div
+        className={
+          isSidebarOpen
+            ? 'sidebar-container '
+            : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 };
