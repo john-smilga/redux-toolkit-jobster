740d8b48755fba3ea864e267ae236fce041eea83
john smilga <johnsmilga@gmail.com>
2022-04-09 11:40:18 -0700

    56-user-location

--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -46,7 +46,10 @@ const jobSlice = createSlice({
       state[name] = value;
     },
     clearValues: () => {
-      return initialState;
+      return {
+        ...initialState,
+        jobLocation: getUserFromLocalStorage()?.location || '',
+      };
     },
   },
   extraReducers: {
--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -7,6 +7,7 @@ import {
   clearValues,
   createJob,
 } from '../../features/job/jobSlice';
+import { useEffect } from 'react';
 const AddJob = () => {
   const {
     isLoading,
@@ -20,6 +21,7 @@ const AddJob = () => {
     isEditing,
     editJobId,
   } = useSelector((store) => store.job);
+  const { user } = useSelector((store) => store.user);
   const dispatch = useDispatch();
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -36,6 +38,15 @@ const AddJob = () => {
     const value = e.target.value;
     dispatch(handleChange({ name, value }));
   };
+
+  useEffect(() => {
+    dispatch(
+      handleChange({
+        name: 'jobLocation',
+        value: user.location,
+      })
+    );
+  }, []);
 
   return (
     <Wrapper>
