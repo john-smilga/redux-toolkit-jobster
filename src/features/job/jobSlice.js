d08fd457b519a4df6a6484c5ad3c96b113d22efe
john smilga <johnsmilga@gmail.com>
2022-04-10 12:22:41 -0700

    68-edit-job-request

--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -57,6 +57,23 @@ export const deleteJob = createAsyncThunk(
   }
 );
 
+export const editJob = createAsyncThunk(
+  'job/editJob',
+  async ({ jobId, job }, thunkAPI) => {
+    try {
+      const resp = await customFetch.patch(`/jobs/${jobId}`, job, {
+        headers: {
+          authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+        },
+      });
+      thunkAPI.dispatch(clearValues());
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
+  }
+);
+
 const jobSlice = createSlice({
   name: 'job',
   initialState,
@@ -90,6 +107,17 @@ const jobSlice = createSlice({
       toast.success(payload);
     },
     [deleteJob.rejected]: (state, { payload }) => {
+      toast.error(payload);
+    },
+    [editJob.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [editJob.fulfilled]: (state) => {
+      state.isLoading = false;
+      toast.success('Job Modified...');
+    },
+    [editJob.rejected]: (state, { payload }) => {
+      state.isLoading = false;
       toast.error(payload);
     },
   },
--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -6,6 +6,7 @@ import {
   handleChange,
   clearValues,
   createJob,
+  editJob,
 } from '../../features/job/jobSlice';
 import { useEffect } from 'react';
 const AddJob = () => {
@@ -28,6 +29,15 @@ const AddJob = () => {
 
     if (!position || !company || !jobLocation) {
       toast.error('Please fill out all fields');
+      return;
+    }
+    if (isEditing) {
+      dispatch(
+        editJob({
+          jobId: editJobId,
+          job: { position, company, jobLocation, jobType, status },
+        })
+      );
       return;
     }
     dispatch(createJob({ position, company, jobLocation, jobType, status }));
