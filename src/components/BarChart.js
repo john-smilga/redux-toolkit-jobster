4d3ef56a04faa30adc70f00bab72d154cc4900fe
john smilga <johnsmilga@gmail.com>
2022-04-11 15:53:57 -0700

    80-area-chart

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,4 +1,24 @@
-const BarChart = () => {
-  return <h2>BarChart</h2>;
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
+
+const BarChartComponent = ({ data }) => {
+  return (
+    <ResponsiveContainer width='100%' height={300}>
+      <BarChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray='10 10 ' />
+        <XAxis dataKey='date' />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Bar dataKey='count' fill='#3b82f6' barSize={75} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
 };
-export default BarChart;
+export default BarChartComponent;
